43825bb23516597cc494bad2f909082c784d11e3
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-05-02 19:37:50 +1000

    feat: movielist component in place

---
+++ src/pages/App.jsx
@@ -0,0 +1,20 @@
+import '../stylesheets/App.css';
+
+function App() {
+
+
+  // 
+
+  return (
+    <div className="App">
+      <h1>Favourite Movies</h1>
+
+      {/* <MovieList /> */}
+
+
+
+    </div>
+  );
+}
+
+export default App;
